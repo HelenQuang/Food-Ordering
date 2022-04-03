12737589c60d9ff02aa33007d22c8335c917d366
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-04-03 22:57:00 +0300

    Add Loading state and Error handling

--- src/Components/Meals/MealsAvailable.js
+++ src/Components/Meals/MealsAvailable.js
@@ -5,16 +5,24 @@ import { useEffect, useState } from "react";
 
 const MealsAvailable = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const res = await fetch(
         "https://food-odering-project-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if (!res.ok) {
+        throw new Error(
+          "Something went wrong! Cannot fetch data for this page!"
+        );
+      }
+
       const data = await res.json();
 
       const loadedMeals = [];
-
       for (const key in data) {
         loadedMeals.push({
           id: key,
@@ -26,10 +34,30 @@ const MealsAvailable = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.loading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section className={styles.loading}>
+        <p>Something went wrong! Cannot fetch data for this page!</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
